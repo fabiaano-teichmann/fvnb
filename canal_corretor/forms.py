0caae6bf887445ad7ee9bbf7df2c93fb611e4458
Fabiano Teichmann <webmaster@newbank.com.br>
2016-10-21 11:48:17 -0200

    formulário completo

--- canal_corretor/forms.py
+++ canal_corretor/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import Autonomo
 
 class AutonomoForm(forms.ModelForm):
-	
 	class Meta:
 		model = Autonomo
 		fields = ["nome", "nasc", "cpf", "creci", "telefone", "email", "site", "phone", "endereco", "cep" ,"cidade",  "email", "estado"]
--- canal_corretor/views.py
+++ canal_corretor/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .forms import AutonomoForm
 from .models import Autonomo
-
+from django.http import HttpResponseRedirect
 
 def index(request):
 	return render(request, 'canal_corretor/index.html', {})
 
 def corretor_new(request):
-	
-	#if request.method == "POST":
-	#	print ( "nome: " + request.POST.get("nome"))
-	#	print ( " cidade:" + request.POST.get("cidade"))
-		#print (request.POST.get("email"))
-		#print (request.POST.get("estado"))
-		#Post.object.create("nome")
-	form = AutonomoForm(request.POST or None)
+	form = AutonomoForm(request.POST or None )
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
+		#depois de salvo limpa o campo nome 
+		instance = form.cleaned_data.get('nome')
 		#  criar um retorno. dizendo que o formulário foi enviado com sucesso
-	return render(request, 'canal_corretor/cad_corretor.html',{'form': form})
+		form = AutonomoForm()
+		
+		return HttpResponseRedirect('/obrigado/')
+	return render(request, 'canal_corretor/cad_corretor.html',{'form': form})	
+			
