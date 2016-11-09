8bb4617e6449a5f8992eea0489871f105093cca4
Fabiano Teichmann <webmaster@newbank.com.br>
2016-11-09 17:04:11 -0200

    atualização fomulário da imobiliaria

--- canal_corretor/forms.py
+++ canal_corretor/forms.py
@@ -18,11 +18,50 @@ class ImobiliariaForm(forms.ModelForm):
 	class Meta:
 		model = Imobiliaria
 		fields = [
-		"img", "razao", "nome", "cnpj", "endereco",
+		"img", "razao","creci_j", "nome", "cnpj", "endereco",
 		"bairro", "cidade","cep","estado", "telefone",
 		"phone", "email", "site","resp", "email_resp","cpf","creci_f"
 
 		]
+		widgets = {
+			'img': forms.FileInput(attrs = {'class': 'form-control'}),
+			'razao':forms.TextInput(attrs ={'class': 'form-control'}),
+			'creci_j': forms.TextInput(attrs ={'class': 'form-control'}),
+			'nome': forms.TextInput(attrs ={'class': 'form-control'}), 
+			'cnpj': forms.TextInput(attrs ={'class': 'form-control'}),
+			'endereco': forms.TextInput(attrs ={'class': 'form-control'}),
+			'bairro': forms.TextInput(attrs ={'class': 'form-control'}),
+			'cidade': forms.TextInput(attrs ={'class': 'form-control'}),
+			'cep': forms.TextInput(attrs ={'class': 'form-control'}),
+			'estado': forms.Select(attrs ={'class': 'form-control'}),
+			'telefone': forms.TextInput(attrs ={'class': 'form-control'}),
+			'phone': forms.TextInput(attrs ={'class': 'form-control'}),
+			'email': forms.EmailInput(attrs ={'class': 'form-control'}),
+			'site': forms.TextInput(attrs ={'class': 'form-control'}),
+			'resp': forms.TextInput(attrs ={'class': 'form-control'}),
+			'email_resp': forms.EmailInput(attrs ={'class': 'form-control'}),
+			'cpf': forms.TextInput(attrs ={'class': 'form-control'}),
+			'creci_f': forms.TextInput(attrs ={'class': 'form-control'}),	
+
+		}
+		error_messages = {
+			'razao': {'required': 'Este campo é obrigatório'},
+			'creci_j': {'required': 'Este campo é obrigatório'},
+			'nome': {'required': 'Este campo é obrigatório'},
+			'cnpj': {'required': 'Este campo é obrigatório'},
+			'endereco': {'required': 'Este campo é obrigatório'},
+			'bairro': {'required': 'Este campo é obrigatório'},
+			'cidade': {'required': 'Este campo é obrigatório'},
+			'cep' : {'required': 'Este campo é obrigatório'},
+			'estado' : {'required': 'Este campo é obrigatório'},
+			'telefone': {'required': 'Este campo é obrigatório'},
+			'email': {'required': 'Este campo é obrigatório'},
+			'resp': {'required': 'Este campo é obrigatório'},
+			'email_resp': {'required': 'Este campo é obrigatório'},
+			'cpf' : {'required': 'Este campo é obrigatório'},
+			
+
+		}
 #CORRETOR AFILIADO
 class CorretorAfiliadoForm(forms.ModelForm):
 	class Meta:
